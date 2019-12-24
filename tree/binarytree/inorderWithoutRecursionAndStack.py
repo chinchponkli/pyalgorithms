53421f5a554dafa097f1ce05af9867c8acc9821e
Arjit Agarwal <arjit.agarwal@meesho.com>
2019-12-24 11:14:01 +0530

    Add logic for Morris Traversal

--- tree/binarytree/inorderWithoutRecursionAndStack.py
+++ tree/binarytree/inorderWithoutRecursionAndStack.py
@@ -1,3 +1,20 @@
+'''
+1. Initialize current as root 
+2. While current is not NULL
+   If the current does not have left child
+      a) Print current’s data
+      b) Go to the right, i.e., current = current->right
+   Else
+      a) Make current as the right child of the rightmost 
+         node in current's left subtree
+      b) Go to this left child, i.e., current = current->left
+      c) It is possible that you have already created a link and in that case you will get
+        current's left subtree's rightmost node's right as current and you have to break link.
+
+Time Complexity: 
+O(n) If we take a closer look, we can notice that every edge of the tree is traversed at most two times. 
+And in the worst case, the same number of extra edges (as input tree) are created and removed.
+'''
 from tree import Tree
 
 def inorder(root):
