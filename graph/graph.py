6ee7e74f9e7a63d25c760634878c20d5876b8ca3
Arjit Agarwal <arjit.agarwal.1000@gmail.com>
2020-01-27 16:39:47 +0530

    Implement basic graph algorithms

---
+++ graph/graph.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+class UndirectedGraph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def bfs(self, s):
+        visited = dict.fromkeys(self.graph.keys(), False)
+        q = [s]
+        visited[s] = True
+        while q:
+            s = q.pop(0)
+            print (s, end = " ")
+            for i in self.graph[s]:
+                if not visited[i]:
+                    q.append(i)
+                    visited[i] = True
+        print()
+
+    def dfs(self, s):
+        visited = dict.fromkeys(self.graph.keys(), False)
+        self.dfsUtil(s, visited)
+        print()
+
+    def dfsUtil(self, s, visited):
+        visited[s] = True
+        print (s, end = " ")
+        for i in self.graph[s]:
+            if not visited[i]:
+                self.dfsUtil(i, visited)
+
+
+g = UndirectedGraph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(2, 3)
+g.addEdge(1, 4)
+g.addEdge(3, 4)
+
+g.bfs(0)
+g.dfs(0)
